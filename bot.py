7c3d1c1ec911e54be714968ab7b5f43b60ff2f3d
JShimonov <jshimonov@gmail.com>
2020-06-10 15:05:17 -0400

    change to if

--- bot.py
+++ bot.py
@@ -22,8 +22,8 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-    # if message.author == client.user:
-    #     return
+    if message.author == client.user:
+        return
 
     league_status = '-lol'
     if message.content == '-lol':
